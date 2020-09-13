fce4ab44fb63b6b8827791a7bd7c0e03775ddc88
nakagami0522 <sweeeet.gami0522@gmail.com>
2020-09-13 14:54:07 +0900

    モデルを回転させながらの平行移動

--- sampleFBX/Player.cpp
+++ sampleFBX/Player.cpp
@@ -113,27 +113,57 @@ void CPlayer::Update()
 	//m_transform->m_position.x += m_vMove.x;
 	//m_transform->m_position.y += m_vMove.y;
 	//m_transform->m_position.z += m_vMove.z;
-	//m_transform->m_rotate.z = m_roll;
+
+	// モデル姿勢に依存しない平行移動
+	XMFLOAT4X4 mtx = XMFLOAT4X4();
+	XMStoreFloat4x4(&mtx, XMMatrixRotationRollPitchYaw(
+		XMConvertToRadians(m_transform->m_rotate.x),
+		XMConvertToRadians(m_transform->m_rotate.y),
+		XMConvertToRadians(0.f)));
+	float3 forward	= float3(mtx._31, mtx._32, mtx._33);
+	float3 right	= float3(mtx._11, mtx._12, mtx._13);
+	float3 up		= float3(mtx._21, mtx._22, mtx._23);
 
 	if (Input::isPress('W')) {
-		m_transform->m_position += m_transform->m_forward * SPEED;
+		m_transform->m_position += forward * SPEED;
 	}
 	if (Input::isPress('S')) {
-		m_transform->m_position -= m_transform->m_forward * SPEED;
+		m_transform->m_position -= forward * SPEED;
 	}
 	if (Input::isPress('D')) {
-		m_transform->m_position += m_transform->m_right * SPEED;
+		m_transform->m_position += right * SPEED;
+		if (m_roll > -MAX_ANGLE_Z)
+			m_roll -= VAL_ANGLE_Z;
+		if (m_roll > 0) {
+			m_roll -= VAL_ANGLE_Z;
+		}
 	}
 	if (Input::isPress('A')) {
-		m_transform->m_position -= m_transform->m_right * SPEED;
+		m_transform->m_position -= right * SPEED;
+		if (m_roll < MAX_ANGLE_Z)
+			m_roll += VAL_ANGLE_Z;
+		if (m_roll < 0) {
+			m_roll += VAL_ANGLE_Z;
+		}
 	}
 	if (Input::isPress(VK_UP)) {
-		m_transform->m_position += m_transform->m_up * SPEED;
+		m_transform->m_position += up * SPEED;
 	}
 	if (Input::isPress(VK_DOWN)) {
-		m_transform->m_position -= m_transform->m_up * SPEED;
+		m_transform->m_position -= up * SPEED;
 	}
 
+	// 移動停止
+	if ((!Input::isPress('A') && !Input::isPress('D'))) {
+		if (m_roll > 0.f) {
+			m_roll -= VAL_ANGLE_Z * 0.5f;
+		}
+		else if (m_roll < 0.f) {
+			m_roll += VAL_ANGLE_Z * 0.5f;
+		}
+	}
+
+	m_transform->m_rotate.z = m_roll;
 
 	// ターゲットロックオン
 	if (Input::isTrigger('T')) {
