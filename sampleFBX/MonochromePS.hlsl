fa53f5f27d98f7c161f2933408eba9b7943e2bb1
nakagami0522 <sweeeet.gami0522@gmail.com>
2020-11-18 15:45:19 +0900

    モノクロシェーダの追加

---
+++ sampleFBX/MonochromePS.hlsl
@@ -0,0 +1,26 @@
+/**
+ * MonochromePS.hlsl
+ */
+
+Texture2D    g_texture		: register(t0);	// テクスチャ
+SamplerState g_sampler		: register(s0);	// サンプラ
+
+// パラメータ
+struct VS_OUTPUT {
+	float4	Pos			: SV_Position;
+	float2	Tex			: TEXCOORD0;
+	float3	Normal		: TEXCOORD1;
+	float3	PosForPS	: TEXCOORD2;
+};
+
+//
+// ピクセルシェーダ
+//
+float4 main(VS_OUTPUT input) : SV_Target0
+{
+	float4 color;
+	color = g_texture.Sample(g_sampler, input.Tex);
+	float mono = 0.299f * color.r + 0.587f * color.g + 0.114f * color.b;
+	color.rgb = mono;
+	return color;
+}
